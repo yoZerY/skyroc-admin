e73380e3242be2766e69dfbe77f9ba18944fa744
Ohh-889 <1509326266@qq.com>
2025-12-10 22:21:48 +0800

    feat(vite): add vite-plugin-progress for build progress tracking

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -1,13 +1,17 @@
 import { devtools } from '@tanstack/devtools-vite';
 import react from '@vitejs/plugin-react';
+import progress from 'vite-plugin-progress';
 
 import { setupAutoImport } from './auto-import';
 import { setupRouterPlugins } from './router';
+import { setupUnocss } from './unocss';
+import { setupUnPluginIcon } from './unplugin-icon';
 
 export function setupVitePlugins(viteEnv: Env.ImportMeta, buildTime: string) {
   return [
     /** - TanStack DevTools */
     devtools(),
+
     /** - TanStack Router */
     setupRouterPlugins(),
     /** - React */
@@ -16,7 +20,9 @@ export function setupVitePlugins(viteEnv: Env.ImportMeta, buildTime: string) {
         plugins: ['babel-plugin-react-compiler']
       }
     }),
-    /** - Auto Import */
-    setupAutoImport()
+    // setupUnocss(viteEnv),
+    // ...setupUnPluginIcon(viteEnv),
+    setupAutoImport(),
+    progress()
   ];
 }
