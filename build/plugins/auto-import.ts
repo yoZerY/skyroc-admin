2db04d9dda4b90411a9a73559c8139a34e67226a
Ohh-889 <1509326266@qq.com>
2025-12-10 21:40:39 +0800

    feat(dependencies): add @iconify/react & @unocss/preset-icons, update vite-plugin-svg-icons

--- build/plugins/auto-import.ts
+++ build/plugins/auto-import.ts
@@ -1,11 +1,23 @@
 import AutoImport from 'unplugin-auto-import/vite';
 
+/**
+ * TanStack Router 的 autoCodeSplitting 会把组件拆成虚拟模块
+ * 虚拟模块 ID 格式：原文件路径?tsr-split=component
+ *
+ * 原本的 /\.[tj]sx?$/ 要求文件 ID 以 .ts/.tsx/.js/.jsx 结尾
+ * 但虚拟模块 ID 如 "index.tsx?tsr-split=component" 以 query string 结尾
+ *
+ * 修改为：/\.[tj]sx?(\?.*)?$/ 可匹配带或不带 query string 的情况
+ */
+const TSR_SPLIT_RE = /\.[tj]sx?(\?.*)?$/;
+
 export function setupAutoImport() {
   return AutoImport({
     dirs: ['src/hooks/**', 'src/components/**'],
     dts: 'src/types/auto-imports.d.ts',
+    dumpUnimportItems: true,
     imports: ['react', { from: 'react', imports: ['FC'], type: true }],
-    include: [/\.[tj]sx?$/],
+    include: [TSR_SPLIT_RE],
     resolvers: [autoImportAntd]
   });
 }
