347c7358d96957ce6a8c0518a7622a0f7edaf17b
Ohh-889 <1509326266@qq.com>
2025-12-14 17:09:33 +0800

    feat(project): add EN/ZH login & error messages for routes

--- src/locales/langs/en-us/route.ts
+++ src/locales/langs/en-us/route.ts
@@ -3,7 +3,14 @@ const route: I18n.Schema['translation']['route'] = {
   home: 'Home',
   notFound: 'Not Found',
   root: 'Root',
-  test: 'Test'
+  login: 'Login',
+  'login_code-login': 'Code Login',
+  login_register: 'Register Account',
+  'login_reset-pwd': 'Reset Password',
+  'login-out': 'Login Out',
+  '403': 'No Permission',
+  '404': 'Page Not Found',
+  '500': 'Server Error'
 };
 
 export default route;
--- src/locales/langs/zh-cn/route.ts
+++ src/locales/langs/zh-cn/route.ts
@@ -3,7 +3,14 @@ const route: I18n.Schema['translation']['route'] = {
   home: '首页',
   notFound: '页面不存在',
   root: '首页',
-  test: '测试'
+  login: '登录',
+  'login_code-login': '验证码登录',
+  login_register: ' 注册账号',
+  'login_reset-pwd': '重置密码',
+  'login-out': '退出登录',
+  '403': '无权限',
+  '404': '页面不存在',
+  '500': '服务器错误'
 };
 
 export default route;
