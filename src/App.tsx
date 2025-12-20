dc8ad19cda1d2704aa443e3b86356109337b3e86
Ohh-889 <1509326266@qq.com>
2025-12-20 12:49:27 +0800

    feat(project): integrate Jotai devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query';
+import { useAtomsDevtools } from 'jotai-devtools';
 
 import LazyAnimate from './features/animate/LazyMotion';
 import AntdContextHolder from './features/antd/AntdContextHolder';
@@ -6,23 +7,32 @@ import AntdProvider from './features/antd/AntdProvider';
 import Devtools from './features/effects/Devtools';
 import GlobalEffect from './features/effects/GlobalEffect';
 import JotaiProvider from './features/jotai/JotaiProvider';
+import { globalStore } from './features/jotai/store';
 import RouterProvider from './features/router/RouterProvider';
 import { queryClient } from './service/queryClient';
 
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  useAtomsDevtools('demo', { store: globalStore });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <JotaiProvider>
+        <Devtools />
+        {children}
+      </JotaiProvider>
+    </QueryClientProvider>
+  );
+};
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <JotaiProvider>
-      <AntdProvider>
-        <AntdContextHolder>
-          <LazyAnimate>
-            <RouterProvider />
-            <GlobalEffect />
-            <Devtools />
-          </LazyAnimate>
-        </AntdContextHolder>
-      </AntdProvider>
-    </JotaiProvider>
-  </QueryClientProvider>
+  <Provider>
+    <AntdProvider>
+      <AntdContextHolder>
+        <LazyAnimate>
+          <RouterProvider />
+          <GlobalEffect />
+        </LazyAnimate>
+      </AntdContextHolder>
+    </AntdProvider>
+  </Provider>
 );
 
 export default App;
