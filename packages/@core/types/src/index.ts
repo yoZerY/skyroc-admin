1ea9d8e28bad099cf75929caa7cb9c7dad45566a
Ohh-889 <1509326266@qq.com>
2026-04-22 23:47:48 +0800

    fix(types): update tsconfig.json to include admin-theme types and add tsbuildinfo file for improved TypeScript build management

--- packages/@core/types/src/index.ts
+++ packages/@core/types/src/index.ts
@@ -3,7 +3,6 @@ export * from './api/service';
 export * from './app/common';
 export * from './app/global';
 export * from './app/menu';
-export * from '../../../web/theme/src/types/theme';
 export * from './app/union-key';
 export * from './app/router';
 export * from './app/storage';
