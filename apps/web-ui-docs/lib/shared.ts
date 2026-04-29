1722bb11a80de08aa39bd16ccafc484635cab964
Ohh-889 <1509326266@qq.com>
2026-04-29 10:29:02 +0800

    chore(shared): update app name and GitHub configuration details

--- apps/web-ui-docs/lib/shared.ts
+++ apps/web-ui-docs/lib/shared.ts
@@ -1,11 +1,11 @@
-export const appName = 'My App';
+export const appName = 'Skyroc UI';
 export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
+  user: 'soybeanjs',
+  repo: 'soybean-admin-react',
   branch: 'main',
 };
