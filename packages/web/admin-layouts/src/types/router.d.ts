01b7467df6eff0f3ca46efc388a404d691d0aeda
Ohh-889 <1509326266@qq.com>
2026-05-10 06:06:31 +0800

    feat(admin-layouts): add router types

---
+++ packages/web/admin-layouts/src/types/router.d.ts
@@ -0,0 +1,9 @@
+// oxlint-disable unicorn/require-module-specifiers
+// oxlint-disable import/no-empty-named-blocks
+import type {} from '@tanstack/react-router';
+
+declare module '@tanstack/react-router' {
+  interface StaticDataRouteOption extends Router.Meta {}
+}
+
+export {};
